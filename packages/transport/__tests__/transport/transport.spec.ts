7efbb1f9d9f742945d29ee46fe1d8f0cbc7fabf4
Matthew Black <mblackmblackmblack@gmail.com>
2021-04-02 15:27:24 -0400

    --wip-- [skip ci]

--- packages/transport/__tests__/transport/transport.spec.ts
+++ packages/transport/__tests__/transport/transport.spec.ts
@@ -1,7 +1,20 @@
 import { expect } from 'chai';
+import * as irc from 'irc';
+import { sleep } from '@liquality/utils';
 
 describe('transport', () => {
   it('should', async () => {
     expect(1).to.equal(1);
+
+    const client = new irc.Client('irc.darkscience.net', 'aslkdjflajfas', {
+      debug: true,
+      channels: ['#darkscience'],
+    });
+
+    client.addListener('message', function (from, to, message) {
+      console.log(from + ' => ' + to + ': ' + message);
+    });
+
+    await sleep(10000);
   });
 });
