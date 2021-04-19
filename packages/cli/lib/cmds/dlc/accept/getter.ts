ff2beed593d545d067929198e5ece2d4db356e08
Matthew Black <mblackmblackmblack@gmail.com>
2021-04-19 19:45:19 -0400

    Add core functionality to CLI
    - Fixes
      - Fix Wallet `create` apikey usage and update to use new @node-dlc/daemon `Endpoint`
    - Changes
      - DlcdClient:
        - Update ApiPrefix to use `api` and `api/v0`
        - Use Axios Basic Http Auth
        - Modify error handling to match Daemon
    - Features
      - Added the following commands
        - Wallet:
          - `walletbalance` => Get Wallet Balance
          - `newaddress` => Get New Address
        - ContractInfo:
          - `decoderawcontractinfo [contractinfo]` => Decode Raw ContractInfo
        - Order:
          - `createdlcorderoffer [contractinfo] [collateral] [feerate] [locktime] [refundlocktime]` => Create Dlc Order Offer
          - `createdlcorderaccept [orderoffer]` => Create Dlc Order Accept
          - `decoderaworderoffer [orderoffer]` => Decode Raw OrderOffer
          - `decoderaworderaccept [orderaccept]` => Decode Raw OrderAccept
        - Dlc:
          - `createdlcoffer [contractinfo] [collateral] [feerate] [locktime] [refundlocktime]` => Create Dlc Offer
          - `acceptdlcoffer [dlcoffer]` => Accept DlcOffer
          - `acceptdlcofferfromfile [filepath]` => Accept DlcOffer From File
          - `signdlcaccept [dlcaccept]` => Sign Dlc Accept
          - `signdlcacceptfromfile [filepath]` => Sign Dlc Accept From File
          - `finalizedlcsign [dlcsign]` => Finalize Dlc Sign
          - `finalizedlcsignfromfile [filepath]` => Finalize Dlc Sign From File
          - `getdlcoffer [tempcontractid]` => Get DlcOffer
          - `getdlcaccept [contractid]` => Get DlcAccept
          - `getdlcsign [contractid]` => Get DlcOffer
          - `getdlcfundingtx [contractid]` => Get Dlc Funding Tx
          - `getdlcexecutetx [contractid] [oracleattestation]` => Get Dlc Execute Tx
          - `getdlcrefundtx [contractid]` => Get Dlc Refund Tx
          - `decoderawdlcoffer [dlcoffer]` => Decode Raw DlcOffer
          - `decoderawdlcaccept [dlcaccept]` => Decode Raw DlcAccept
          - `decoderawdlcacceptfromfile [filepath]` => Decode Raw DlcAccept From File
        - Oracle:
          - `decoderaworacleannouncement [oracleannouncement]` => Decode Raw OracleAnnouncement
          - `decoderaworacleattestation [oracleattestation]` => Decode Raw OracleAttestation
    - Tests
      - Added `DlcdClient` spec

---
+++ packages/cli/lib/cmds/dlc/accept/getter.ts
@@ -0,0 +1,23 @@
+import { Logger } from '@node-dlc/logger';
+import DlcdClient from '../../../client/DlcdClient';
+import { getLogger } from '../../../utils/config';
+import { IArguments } from '../../../arguments';
+import { Endpoint } from '@node-dlc/daemon';
+
+export const command = 'getdlcaccept [contractid]';
+
+export const describe = 'Get DlcAccept';
+
+export const builder = {
+  apikey: {
+    default: '',
+  },
+};
+
+export async function handler(argv: IArguments): Promise<void> {
+  const { host, port, apikey, loglevel, contractid } = argv;
+  const logger: Logger = getLogger(loglevel);
+  const client = new DlcdClient(host, port, logger, apikey, 'api/v0');
+  const response = await client.get(`${Endpoint.DlcAccept}/${contractid}`);
+  logger.log(response.hex);
+}
