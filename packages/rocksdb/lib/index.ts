982e606832eb6f9e684f18c7ba38e95cf86b7481
Matthew Black <mblackmblackmblack@gmail.com>
2021-05-21 00:12:43 -0400

    Ensure rocksdb oracle store exported

--- packages/rocksdb/lib/index.ts
+++ packages/rocksdb/lib/index.ts
@@ -2,3 +2,4 @@ export * from './rocksdb-wallet-store';
 export * from './rocksdb-order-store';
 export * from './rocksdb-dlc-store';
 export * from './rocksdb-irc-store';
+export * from './rocksdb-oracle-store';
