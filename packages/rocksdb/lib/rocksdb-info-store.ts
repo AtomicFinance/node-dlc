e836580d3c64db62a05d1e7e3c1f628bab45ec6c
Matthew Black <mblackmblackmblack@gmail.com>
2021-10-21 01:41:41 -0400

    Add increment dlc info values

--- packages/rocksdb/lib/rocksdb-info-store.ts
+++ packages/rocksdb/lib/rocksdb-info-store.ts
@@ -5,6 +5,14 @@ enum Prefix {
   DlcInfoV0 = 90,
 }
 
+export type InfoValue =
+  | 'offer'
+  | 'accept'
+  | 'sign'
+  | 'cancel'
+  | 'close'
+  | 'transactions';
+
 export class RocksdbInfoStore extends RocksdbBase {
   public async findDlcInfo(): Promise<DlcInfoV0> {
     const key = Buffer.concat([Buffer.from([Prefix.DlcInfoV0])]);
@@ -22,5 +30,32 @@ export class RocksdbInfoStore extends RocksdbBase {
   public async deleteDlcInfo(): Promise<void> {
     const key = Buffer.concat([Buffer.from([Prefix.DlcInfoV0])]);
     await this._db.del(key);
+  }
+
+  public async incrementDlcInfoValues(values: InfoValue[]): Promise<void> {
+    const dlcInfo = await this.findDlcInfo();
+    for (const value of values) {
+      switch (value) {
+        case 'offer':
+          dlcInfo.numDlcOffers += 1;
+          break;
+        case 'accept':
+          dlcInfo.numDlcAccepts += 1;
+          break;
+        case 'sign':
+          dlcInfo.numDlcSigns += 1;
+          break;
+        case 'cancel':
+          dlcInfo.numDlcCancels += 1;
+          break;
+        case 'close':
+          dlcInfo.numDlcCloses += 1;
+          break;
+        case 'transactions':
+          dlcInfo.numDlcTransactions += 1;
+          break;
+      }
+    }
+    await this.saveDlcInfo(dlcInfo);
   }
 }
