08c75cbcc05db45e1288f78f65ab34efeab942fb
Matthew Black <mblackmblackmblack@gmail.com>
2022-06-27 21:05:21 -0400

    --wip-- [skip ci]

--- packages/messaging/lib/messages/PayoutFunction.ts
+++ packages/messaging/lib/messages/PayoutFunction.ts
@@ -9,6 +9,9 @@ import {
   PolynomialPayoutCurvePieceJSON,
 } from './PayoutCurvePiece';
 
+export enum PayoutFunctionType {
+  PayoutFunction = 0,
+}
 export abstract class PayoutFunction {
   public static deserialize(buf: Buffer): PayoutFunctionV0 {
     const reader = new BufferReader(buf);
@@ -16,7 +19,7 @@ export abstract class PayoutFunction {
     const type = Number(reader.readBigSize());
 
     switch (type) {
-      case MessageType.PayoutFunctionV0:
+      case PayoutFunctionType.PayoutFunction:
         return PayoutFunctionV0.deserialize(buf);
       default:
         throw new Error(`Payout function TLV type must be PayoutFunctionV0`);
@@ -61,9 +64,11 @@ export class PayoutFunctionV0 extends PayoutFunction implements IDlcMessage {
 
       instance.pieces.push({
         payoutCurvePiece,
-        endpoint,
-        endpointPayout,
-        extraPrecision,
+        endpoint: {
+          eventOutcome: endpoint,
+          outcomePayout: endpointPayout,
+          extraPrecision,
+        },
       });
     }
 
@@ -81,6 +86,8 @@ export class PayoutFunctionV0 extends PayoutFunction implements IDlcMessage {
   public endpointPayout0: bigint;
   public extraPrecision0: number;
 
+  public lastEndpoint: IEndpoint;
+
   public pieces: IPayoutCurvePieces[] = [];
 
   /**
@@ -88,16 +95,19 @@ export class PayoutFunctionV0 extends PayoutFunction implements IDlcMessage {
    */
   public toJSON(): PayoutFunctionV0JSON {
     return {
-      type: this.type,
-      endpoint0: Number(this.endpoint0),
-      endpointPayout0: Number(this.endpointPayout0),
-      extraPrecision0: this.extraPrecision0,
-      pieces: this.pieces.map((piece) => {
+      lastEndpoint: {
+        eventOutcome: Number(this.endpoint0),
+        outcomePayout: Number(this.endpointPayout0),
+        extraPrecision: this.extraPrecision0,
+      },
+      payoutFunctionPieces: this.pieces.map((piece) => {
         return {
           payoutCurvePiece: piece.payoutCurvePiece.toJSON(),
-          endpoint: Number(piece.endpoint),
-          endpointPayout: Number(piece.endpointPayout),
-          extraPrecision: piece.extraPrecision,
+          endpoint: {
+            eventOutcome: Number(piece.endpoint.eventOutcome),
+            outcomePayout: Number(piece.endpoint.outcomePayout),
+            extraPrecision: piece.endpoint.extraPrecision,
+          },
         };
       }),
     };
@@ -118,9 +128,9 @@ export class PayoutFunctionV0 extends PayoutFunction implements IDlcMessage {
 
     for (const piece of this.pieces) {
       dataWriter.writeBytes(piece.payoutCurvePiece.serialize());
-      dataWriter.writeBigSize(piece.endpoint);
-      dataWriter.writeBigSize(piece.endpointPayout);
-      dataWriter.writeUInt16BE(piece.extraPrecision);
+      dataWriter.writeBigSize(piece.endpoint.eventOutcome);
+      dataWriter.writeBigSize(piece.endpoint.outcomePayout);
+      dataWriter.writeUInt16BE(piece.endpoint.extraPrecision);
     }
 
     writer.writeBigSize(dataWriter.size);
@@ -130,124 +140,31 @@ export class PayoutFunctionV0 extends PayoutFunction implements IDlcMessage {
   }
 }
 
-/**
- * PayoutFunction V0
- */
-export class PayoutFunctionOldV0 extends PayoutFunction implements IDlcMessage {
-  public static type = MessageType.PayoutFunctionV0;
+interface IEndpoint {
+  eventOutcome: bigint;
+  outcomePayout: bigint;
+  extraPrecision: number;
+}
 
-  /**
-   * Deserializes an payout_function_v0 message
-   * @param buf
-   */
-  public static deserialize(buf: Buffer): PayoutFunctionV0 {
-    const instance = new PayoutFunctionV0();
-    const reader = new BufferReader(buf);
-
-    reader.readBigSize(); // read type
-    instance.length = reader.readBigSize(); // need to fix this
-    reader.readUInt16BE(); // num_pieces
-    instance.endpoint0 = reader.readBigSize();
-    instance.endpointPayout0 = reader.readBigSize();
-    instance.extraPrecision0 = reader.readUInt16BE();
-
-    while (!reader.eof) {
-      const payoutCurvePiece = PayoutCurvePiece.deserialize(getTlv(reader));
-      const endpoint = reader.readBigSize();
-      const endpointPayout = reader.readBigSize();
-      const extraPrecision = reader.readUInt16BE();
-
-      instance.pieces.push({
-        payoutCurvePiece,
-        endpoint,
-        endpointPayout,
-        extraPrecision,
-      });
-    }
-
-    return instance;
-  }
-
-  /**
-   * The type for payout_function_v0 message. payout_function_v0 = 42790
-   */
-  public type = PayoutFunctionV0.type;
-
-  public length: bigint;
-
-  public endpoint0: bigint;
-  public endpointPayout0: bigint;
-  public extraPrecision0: number;
-
-  public pieces: IPayoutCurvePieces[] = [];
-
-  /**
-   * Converts payout_function_v0 to JSON
-   */
-  public toJSON(): PayoutFunctionV0JSON {
-    return {
-      type: this.type,
-      endpoint0: Number(this.endpoint0),
-      endpointPayout0: Number(this.endpointPayout0),
-      extraPrecision0: this.extraPrecision0,
-      pieces: this.pieces.map((piece) => {
-        return {
-          payoutCurvePiece: piece.payoutCurvePiece.toJSON(),
-          endpoint: Number(piece.endpoint),
-          endpointPayout: Number(piece.endpointPayout),
-          extraPrecision: piece.extraPrecision,
-        };
-      }),
-    };
-  }
-
-  /**
-   * Serializes the payout_function_v0 message into a Buffer
-   */
-  public serialize(): Buffer {
-    const writer = new BufferWriter();
-    writer.writeBigSize(this.type);
-
-    const dataWriter = new BufferWriter();
-    dataWriter.writeUInt16BE(this.pieces.length);
-    dataWriter.writeBigSize(this.endpoint0);
-    dataWriter.writeBigSize(this.endpointPayout0);
-    dataWriter.writeUInt16BE(this.extraPrecision0);
-
-    for (const piece of this.pieces) {
-      dataWriter.writeBytes(piece.payoutCurvePiece.serialize());
-      dataWriter.writeBigSize(piece.endpoint);
-      dataWriter.writeBigSize(piece.endpointPayout);
-      dataWriter.writeUInt16BE(piece.extraPrecision);
-    }
-
-    writer.writeBigSize(dataWriter.size);
-    writer.writeBytes(dataWriter.toBuffer());
-
-    return writer.toBuffer();
-  }
+interface IEndpointJSON {
+  eventOutcome: number;
+  outcomePayout: number;
+  extraPrecision: number;
 }
 
 interface IPayoutCurvePieces {
   payoutCurvePiece: PayoutCurvePiece;
-  endpoint: bigint;
-  endpointPayout: bigint;
-  extraPrecision: number;
+  endpoint: IEndpoint;
 }
 
 interface IPayoutCurvePiecesJSON {
   payoutCurvePiece:
     | PolynomialPayoutCurvePieceJSON
     | HyperbolaPayoutCurvePieceJSON;
-  endpoint: number;
-  endpointPayout: number;
-  extraPrecision: number;
+  endpoint: IEndpointJSON;
 }
 
 export interface PayoutFunctionV0JSON {
-  type: number;
-  endpoint0: number;
-  endpointPayout0: number;
-  extraPrecision0: number;
-  pieces: IPayoutCurvePiecesJSON[];
+  payoutFunctionPieces: IPayoutCurvePiecesJSON[];
+  lastEndpoint: IEndpointJSON;
 }
