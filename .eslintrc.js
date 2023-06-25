f7047189424f0a5c3111e99abaea6bb6ae4dcfac
Matthew Black <mblackmblackmblack@gmail.com>
2023-06-25 15:10:59 -0400

    👽️ compat: improve env compability with bigint
    bigint literals (`n` notation, i.e. 10n) isn't compatible with all
    environments
    
    switches bigint literals to `BigInt` function notation to improve env
    compatibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,4 +19,27 @@ module.exports = {
     'prettier/prettier': 'error',
     'simple-import-sort/imports': 'error',
   },
+  overrides: [
+    {
+      // Apply this rule to all files
+      files: ['**/*'],
+      rules: {
+        'no-restricted-syntax': [
+          'error',
+          {
+            selector: 'Literal[bigint]',
+            message:
+              'Avoid using bigint literals. Please use BigInt function notation instead. E.g., BigInt("123") instead of 123n.',
+          },
+        ],
+      },
+    },
+    {
+      // Exclude this rule for test files (*.test.js, *.test.ts, *.spec.js, *.spec.ts)
+      files: ['**/*.test.js', '**/*.test.ts', '**/*.spec.js', '**/*.spec.ts'],
+      rules: {
+        'no-restricted-syntax': 'off',
+      },
+    },
+  ],
 };
